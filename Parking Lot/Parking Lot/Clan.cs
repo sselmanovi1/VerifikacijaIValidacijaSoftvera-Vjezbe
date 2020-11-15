72bb368d43ae7677e9f3dadaf37c5c58bf58b1b4
Ehlimana <ekrupalija1@etf.unsa.ba>
2020-11-15 20:35:10 +0100

    Commit programskog koda

---
+++ Parking Lot/Parking Lot/Clan.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq;
+
+namespace Parking_Lot
+{
+    public enum Status
+    {
+        Aktivna,
+        Neaktivna
+    }
+
+    public class Clan : Korisnik
+    {
+        #region Atributi
+
+        string password;
+        Status status;
+        DateTime aktivnaDo;
+        Tuple<int, Lokacija> rezervisanoParkingMjesto;
+
+        #endregion
+
+        #region Properties
+
+        public string Password
+        {
+            get => password;
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value) || value.Length < 10
+                    || !value.All(char.IsLetterOrDigit))
+                    throw new FormatException("Neispravan format passworda!");
+
+                password = value;
+            }
+        }
+
+        public Status Status
+        {
+            get => status;
+        }
+
+        public DateTime AktivnaDo
+        {
+            get => aktivnaDo;
+        }
+
+        public Tuple<int, Lokacija> RezervisanoParkingMjesto
+        {
+            get => rezervisanoParkingMjesto;
+            set => rezervisanoParkingMjesto = value;
+        }
+
+        #endregion
+
+        #region Konstruktor
+
+        public Clan(string user, string pass, string address, Vozilo vehicle, DateTime endDate)
+            : base(user, address, vehicle)
+        {
+            Password = pass;
+            aktivnaDo = endDate;
+            status = Status.Aktivna;
+        }
+
+        public Clan(DateTime endDate)
+        {
+            aktivnaDo = endDate;
+            status = Status.Aktivna;
+        }
+
+        #endregion
+
+        #region Metode
+
+        public void ProvjeriJeLiČlanarinaIstekla()
+        {
+            if (DateTime.Now > aktivnaDo)
+                status = Status.Neaktivna;
+        }
+
+        /// <summary>
+        /// Metoda u kojoj se vrši produživanje važenja članarine.
+        /// Ukoliko članarina još uvijek nije istekla, potrebno je baciti izuzetak,
+        /// kao i u slučaju da je članarina istekla prije više od godinu dana.
+        /// U suprotnom, odobrava se produženje članarine.
+        /// </summary>
+        public void ProdužiČlanarinu(DateTime noviRok)
+        {
+
+        }
+
+        #endregion
+    }
+}
---
+++ Parking Lot/Parking Lot/Vozilo.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Parking_Lot
+{
+    public class Vozilo
+    {
+
+        #region Atributi
+
+        string vrsta, tablice;
+        int brojSjedišta;
+
+        #endregion
+
+        #region Properties
+
+        public string Vrsta
+        {
+            get => vrsta;
+            set
+            {
+                List<string> vrste = new List<string>() { "Automobil", "Motor", "Autobus", "Kamionet", "Minibus" };
+                if (vrste.Find(element => element == value) == null)
+                    throw new ArgumentException("Nepostojeća vrsta vozila!");
+
+                vrsta = value;
+            }
+        }
+
+        public string Tablice
+        {
+            get => tablice;
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value) || value.Length != 9
+                    || !value.Substring(0, 3).All(char.IsDigit)
+                    || !(value.Substring(3, 1) == "-")
+                    || !value.Substring(4, 1).All(char.IsUpper)
+                    || !(value.Substring(5, 1) == "-")
+                    || !value.Substring(6).All(char.IsDigit))
+                    throw new FormatException("Neispravan format tablica!");
+
+                tablice = value;
+            }
+        }
+
+        public int BrojSjedišta
+        {
+            get => brojSjedišta;
+            set
+            {
+                if (value < 0)
+                    throw new FormatException("Neispravan broj sjedišta!");
+
+                brojSjedišta = value;
+            }
+        }
+
+        #endregion
+
+        #region Konstruktor
+
+        public Vozilo(string type, string plates, int noSeats)
+        {
+            Vrsta = type;
+            Tablice = plates;
+            BrojSjedišta = noSeats;
+        }
+
+        #endregion
+
+        #region Metode
+
+        /// <summary>
+        /// Metoda u kojoj se provjerava ispravnost podataka za vozilo.
+        /// Ovisno o vrsti, dozvoljen je različit broj sjedišta:
+        /// Automobil - 4 do 7 sjedišta
+        /// Motor - 1 do 2 sjedišta
+        /// Autobus - 20 do 100 sjedišta
+        /// Kamionet - 2 do 3 sjedišta
+        /// Minibus - 5 do 20 sjedišta
+        /// Sve ostale kombinacije dovode do pojave izuzetka.
+        /// </summary>
+        /// <returns></returns>
+        public bool ProvjeriIspravnostVozila()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
