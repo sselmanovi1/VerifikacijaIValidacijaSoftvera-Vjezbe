50586664555c115924d2eb2fcce6e2ac1e2466f6
Ehlimana <ekrupalija1@etf.unsa.ba>
2020-12-13 23:58:21 +0100

    Commit programskog koda

---
+++ Grupa RS-3/Tinder.ba/Tinder.ba/Poruka.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tinder.ba
+{
+    public class Poruka
+    {
+        #region Atributi
+
+        Korisnik posiljalac, primalac;
+        string sadrzaj;
+
+        #endregion
+
+        #region Properties
+
+        public Korisnik Posiljalac
+        {
+            get => posiljalac;
+        }
+        public Korisnik Primalac
+        {
+            get => primalac;
+        }
+        public string Sadrzaj
+        {
+            get => sadrzaj;
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value) || value.Contains("pogrdna riječ"))
+                    throw new InvalidOperationException("Neispravan sadržaj poruke!");
+
+                sadrzaj = value;
+            }
+        }
+
+        #endregion
+
+        #region Konstruktor
+
+        public Poruka(Korisnik sender, Korisnik receiver, string content)
+        {
+            if (sender == null || receiver == null)
+                throw new ArgumentNullException("Nedefinisan pošiljalac/primalac!");
+
+            posiljalac = sender;
+            primalac = receiver;
+            Sadrzaj = content;
+        }
+
+        #endregion
+
+        #region Metode
+
+        /// <summary>
+        /// Metoda za izračunavanje potencijala poruke.
+        /// Maksimalni potencijal je 100, a minimalni 0.
+        /// Ukoliko poruka sadrži riječi "bježi", "neću", "oženjen", "udata" ili "neistina" potencijal se smanjuje za 20 po prisutnoj riječi.
+        /// Ukoliko poruka sadrži riječi "volim", "ljubav", "slobodan", "slobodna" ili "hoću" potencijal se povećava za 20 po prisutnoj riječi.
+        /// </summary>
+        /// <returns></returns>
+        public int IzračunajPotencijalPoruke()
+        {
+            string[] rijeci = new string[] {"bježi", "neću", "oženjen", "udata", "neistina",
+                "volim", "ljubav", "slobodan", "slobodna", "hoću"};
+
+            int potencijal = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (i < 5 && sadrzaj.Contains(rijeci[i]))
+                    potencijal -= 20;
+                else if (sadrzaj.Contains(rijeci[i]))
+                    potencijal += 20;
+            }
+
+            if (potencijal < 0)
+                potencijal = 0;
+            else if (potencijal > 100)
+                potencijal = 100;
+
+            return potencijal;
+        }
+
+        #endregion
+    }
+}
---
+++ Grupa RS-3/Tinder.ba/Tinder.ba/Tinder.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tinder.ba
+{
+    public class Tinder
+    {
+        #region Atributi
+
+        List<Korisnik> korisnici;
+        List<Chat> razgovori;
+
+        #endregion
+
+        #region Properties
+
+        public List<Korisnik> Korisnici
+        {
+            get => korisnici;
+        }
+
+        public List<Chat> Razgovori
+        {
+            get => razgovori;
+        }
+
+        #endregion
+
+        #region Konstruktor
+
+        public Tinder()
+        {
+            korisnici = new List<Korisnik>();
+            razgovori = new List<Chat>();
+        }
+
+        #endregion
+
+        #region Metode
+
+        public void RadSaKorisnikom(Korisnik k, int opcija)
+        {
+            if (opcija == 0)
+            {
+                Korisnik postojeci = korisnici.Find(korisnik => korisnik.Ime == k.Ime);
+                if (postojeci != null)
+                    throw new InvalidOperationException("Korisnik već postoji!");
+
+                korisnici.Add(k);
+            }
+            else if (opcija == 1)
+            {
+                Korisnik postojeci = korisnici.Find(korisnik => korisnik.Ime == k.Ime);
+                if (postojeci == null)
+                    throw new InvalidOperationException("Korisnik ne postoji!");
+
+                korisnici.Remove(k);
+
+                List<Chat> razgovoriZaBrisanje = new List<Chat>();
+                foreach (Chat c in razgovori)
+                {
+                    if (c.Korisnici.Find(korisnik => korisnik.Ime == k.Ime) != null)
+                        razgovoriZaBrisanje.Add(c);
+                }
+
+                foreach (Chat brisanje in razgovoriZaBrisanje)
+                    razgovori.Remove(brisanje);
+            }
+        }
+
+        public void DodavanjeRazgovora(List<Korisnik> korisnici, bool grupniChat)
+        {
+            if (korisnici == null || korisnici.Count < 2 || (!grupniChat && korisnici.Count > 2))
+                throw new ArgumentException("Nemoguće dodati razgovor!");
+
+            if (grupniChat)
+                razgovori.Add(new GrupniChat(korisnici));
+
+            else
+                razgovori.Add(new Chat(korisnici[0], korisnici[1]));
+        }
+
+        /// <summary>
+        /// Metoda u kojoj se određuju i vraćaju svi kompatibilni korisnici u listi parova.
+        /// Parovi ne smiju imati duplikate - dva para ne smiju imati ista dva korisnika.
+        /// Ukoliko nema nijednog korisnika, baca se izuzetak.
+        /// Korisnici su kompatibilni ako lokacija k1 odgovara željenoj lokaciji k2 i obrnuto
+        /// i ukoliko se godine k1 nalaze između minimalnih i maksimalnih željenih godina k2 i obrnuto.
+        /// </summary>
+        /// <returns></returns>
+        public List<Tuple<Korisnik, Korisnik>> DajSveKompatibilneKorisnike()
+        {
+            if (Korisnici.Count < 1)
+                throw new Exception("Nema korisnika!");
+
+            List<Tuple<Korisnik, Korisnik>> parovi = new List<Tuple<Korisnik, Korisnik>>();
+            
+            foreach(Korisnik prvi in korisnici)
+            {
+                foreach(Korisnik drugi in korisnici)
+                {
+                    bool postoji = false;
+                    if (prvi == drugi)
+                        postoji = true;
+                    foreach (Tuple<Korisnik, Korisnik> par in parovi)
+                    {
+                        if (par.Item1 == prvi && par.Item2 == drugi)
+                            postoji = true;
+                        else if (par.Item2 == prvi && par.Item1 == drugi)
+                            postoji = true;
+                    }
+                    if (!postoji)
+                    {
+                        if (prvi.ZeljenaLokacija == drugi.Lokacija && prvi.Lokacija == drugi.ZeljenaLokacija
+                            && prvi.Godine >= drugi.ZeljeniMinGodina && prvi.Godine <= drugi.ZeljeniMaxGodina
+                            && drugi.Godine >= prvi.ZeljeniMinGodina && drugi.Godine <= prvi.ZeljeniMaxGodina)
+                            parovi.Add(new Tuple<Korisnik, Korisnik>(prvi, drugi));
+                    }
+                }
+            }
+            List<Tuple<Korisnik, Korisnik>> paroviZaBrisanje = new List<Tuple<Korisnik, Korisnik>>();
+            foreach (Tuple<Korisnik, Korisnik> par in parovi)
+            {
+                if (par.Item1 == par.Item2)
+                    paroviZaBrisanje.Add(par);
+            }
+
+            return parovi;
+        }
+
+        public bool DaLiJeSpajanjeUspjesno(Chat c, IRecenzija r)
+        {
+            if (c is GrupniChat)
+                throw new InvalidOperationException("Grupni chatovi nisu podržani!");
+
+            if (c.Poruke.Find(poruka => poruka.IzračunajPotencijalPoruke() == 100) != null
+                && r.DajUtisak() == "Pozitivan")
+                return true;
+
+            else
+                return false;
+        }
+
+        public int PotencijalChata(Chat c)
+        {
+            if (c is GrupniChat)
+                throw new InvalidOperationException("Grupni chatovi nisu podržani!");
+
+            int potencijal = 0;
+            foreach(Poruka p in c.Poruke)
+            {
+                potencijal += p.IzračunajPotencijalPoruke();
+            }
+
+            return potencijal;
+        }
+        #endregion
+    }
+}
