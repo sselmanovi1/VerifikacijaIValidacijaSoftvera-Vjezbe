e8605a87c9b8d30f3afe17821a0ad7f5a7161347
Ehlimana <ekrupalija1@etf.unsa.ba>
2020-12-13 23:54:41 +0100

    Commit programskog koda

---
+++ Grupa 3/eParking/eParking/Clan.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Linq;
+
+namespace eParking
+{
+    public enum Status
+    {
+        Aktivna,
+        Neaktivna
+    }
+    public class Clan : Korisnik
+    {
+        #region Atributi
+
+        string password;
+        Status status;
+        DateTime aktivnaDo;
+        Tuple<int, Lokacija> rezervisanoParkingMjesto;
+
+        #endregion
+
+        #region Properties
+
+        public string Password
+        {
+            get => password;
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value) || value.Length < 10
+                    || !value.All(char.IsLetterOrDigit))
+                    throw new FormatException("Neispravan format passworda!");
+
+                password = value;
+            }
+        }
+
+        public Status Status
+        {
+            get => status;
+        }
+
+        public DateTime AktivnaDo
+        {
+            get => aktivnaDo;
+        }
+
+        public Tuple<int, Lokacija> RezervisanoParkingMjesto
+        {
+            get => rezervisanoParkingMjesto;
+        }
+
+        #endregion
+
+        #region Konstruktor
+
+        public Clan(string user, string pass, string address, Vozilo vehicle, DateTime endDate)
+            : base(user, address, vehicle)
+        {
+            Password = pass;
+            aktivnaDo = endDate;
+            status = Status.Aktivna;
+        }
+
+        public Clan(DateTime endDate)
+        {
+            aktivnaDo = endDate;
+            status = Status.Aktivna;
+        }
+
+        #endregion
+
+        #region Metode
+
+        /// <summary>
+        /// Metoda u kojoj se vrši rezervacija parking mjesta za korisnika.
+        /// Ako je osobi istekla članarina, dolazi do pojave izuzetka, kao i u slučaju
+        /// da već postoji rezervisano parking mjesto.
+        /// </summary>
+        /// <param name="l"></param>
+        public void RezervišiMjesto(Lokacija l)
+        {
+            ProvjeriJeLiČlanarinaIstekla();
+            if (status == Status.Neaktivna || rezervisanoParkingMjesto != null)
+                throw new Exception("Istekla članarina/već postoji rezervisano mjesto!");
+
+            rezervisanoParkingMjesto = new Tuple<int, Lokacija>(l.DajTrenutniBrojSlobodnogMjesta(), l);
+        }
+
+        public void ProvjeriJeLiČlanarinaIstekla()
+        {
+            if (DateTime.Now > aktivnaDo)
+                status = Status.Neaktivna;
+        }
+
+        public void OtkažiRezervaciju()
+        {
+            rezervisanoParkingMjesto = null;
+        }
+
+        #endregion
+    }
+}
---
+++ Grupa 3/eParking/eParking/Vozilo.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace eParking
+{
+    public class Vozilo
+    {
+
+        #region Atributi
+
+        string vrsta, tablice;
+        int brojSjedišta;
+
+        #endregion
+
+        #region Properties
+
+        public string Vrsta
+        {
+            get => vrsta;
+            set
+            {
+                List<string> vrste = new List<string>() { "Automobil", "Motor", "Autobus", "Kamionet", "Minibus" };
+                if (vrste.Find(element => element == value) == null)
+                    throw new ArgumentException("Nepostojeća vrsta vozila!");
+
+                vrsta = value;
+            }
+        }
+
+        public string Tablice
+        {
+            get => tablice;
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value) || value.Length != 9
+                    || !value.Substring(0, 3).All(char.IsDigit)
+                    || !(value.Substring(3, 1) == "-")
+                    || !value.Substring(4, 1).All(char.IsUpper)
+                    || !(value.Substring(5, 1) == "-")
+                    || !value.Substring(6).All(char.IsDigit))
+                    throw new FormatException("Neispravan format tablica!");
+
+                tablice = value;
+            }
+        }
+
+        public int BrojSjedišta
+        {
+            get => brojSjedišta;
+            set
+            {
+                if (value < 0)
+                    throw new FormatException("Neispravan broj sjedišta!");
+
+                brojSjedišta = value;
+            }
+        }
+
+        #endregion
+
+        #region Konstruktor
+
+        public Vozilo(string type, string plates, int noSeats)
+        {
+            Vrsta = type;
+            Tablice = plates;
+            BrojSjedišta = noSeats;
+        }
+
+        #endregion
+
+    }
+}
