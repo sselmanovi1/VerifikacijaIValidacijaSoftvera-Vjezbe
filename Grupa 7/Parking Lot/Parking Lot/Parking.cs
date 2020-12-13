ce32fe7336a4d9c22e840e7cf095bf07affab6f5
Ehlimana <ekrupalija1@etf.unsa.ba>
2020-12-13 23:56:49 +0100

    Commit programskog koda

---
+++ Grupa 7/Parking Lot/Parking Lot/Parking.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parking_Lot
+{
+    public class Parking
+    {
+        #region Atributi
+
+        List<Korisnik> korisnici;
+        List<Lokacija> lokacije;
+
+        #endregion
+
+        #region Properties
+
+        public List<Korisnik> Korisnici
+        {
+            get => korisnici;
+        }
+
+        public List<Lokacija> Lokacije
+        {
+            get => lokacije;
+        }
+
+        #endregion
+
+        #region Konstruktor
+
+        public Parking()
+        {
+            korisnici = new List<Korisnik>();
+            lokacije = new List<Lokacija>();
+        }
+
+        #endregion
+
+        #region Metode
+
+        public void RadSaLokacijom(Lokacija l, int opcija, List<string> podaci = null)
+        {
+            if (opcija == 0)
+            {
+                if (Lokacije.Find(lokacija => lokacija.Naziv == l.Naziv) != null)
+                    throw new InvalidOperationException("Lokacija već postoji!");
+
+                Lokacije.Add(l);
+            }
+            else if (opcija == 1)
+            {
+                Lokacija lokacija = Lokacije.Find(loc => loc.Naziv == l.Naziv);
+                if (lokacija == null)
+                    throw new InvalidOperationException("Lokacija ne postoji!");
+
+                Lokacije.Remove(lokacija);
+            }
+            else if (opcija == 2)
+            {
+                Lokacija lokacija = Lokacije.Find(loc => loc.Naziv == l.Naziv);
+                if (lokacija == null)
+                    throw new InvalidOperationException("Lokacija ne postoji!");
+
+                foreach (string ulica in podaci)
+                    if (!lokacija.Ulice.Contains(ulica))
+                        lokacija.Ulice.Add(ulica);
+            }
+        }
+
+        public void DodajKorisnika(Korisnik k, bool clan)
+        {
+            Korisnik korisnik = null;
+            foreach (Korisnik kor in Korisnici)
+            {
+                if (kor.Username == k.Username)
+                    korisnik = kor;
+            }
+
+            if (korisnik != null && !clan)
+                throw new ArgumentException("Korisnik već postoji!");
+            else if (korisnik != null)
+            {
+                foreach (Korisnik kor in Korisnici)
+                {
+                    if (kor.Username == k.Username)
+                        korisnik = kor;
+                }
+                Korisnici.Remove(korisnik);
+                Korisnici.Add(k);
+            }
+            else
+                Korisnici.Add(k);
+        }
+
+        public void RezervišiParking(Clan c, Lokacija l)
+        {
+            Tuple<int, Lokacija> pm = new Tuple<int, Lokacija>(l.DajTrenutniBrojSlobodnogMjesta(), l);
+            c.RezervisanoParkingMjesto = pm;
+        }
+
+        public void OslobodiParkingMjesto(ITransakcija transakcija, Clan c)
+        {
+            if (transakcija.DajVrijemeDolaska(c.Vozilo).AddHours(24) < DateTime.Now)
+            {
+                c.RezervisanoParkingMjesto.Item2.OslobodiMjesto();
+                c.RezervisanoParkingMjesto = null;
+            }
+            else
+                throw new InvalidOperationException("Još uvijek nisu prošla 24 sata!");
+        }
+
+        /// <summary>
+        /// Metoda u kojoj se vrši pretvaranje korisnika u člana.
+        /// Prvo je potrebno pronaći postojećeg korisnika sa poslanim korisničkim imenom.
+        /// Ukoliko korisnik ne postoji, baca se izuzetak.
+        /// Podacima od pronađenog korisnika dodaje se nasumično generisani password sa
+        /// ispravnim formatom prema postojećoj programskoj logici, a članarina mu se
+        /// odobrava na narednih godinu dana. Zatim se password vraća kao rezultat metode.
+        /// </summary>
+        /// <param name="username"></param>
+        public string DodavanjeČlana(string username)
+        {
+            Korisnik k = Korisnici.Find(korisnik => korisnik.Username == username);
+            if (k == null)
+                throw new Exception("Član ne postoji!");
+
+            Random rand = new Random();
+            string password = "";
+            for (int i = 0; i < 15; i++)
+                password += rand.Next(10).ToString();
+
+            Clan c = new Clan(k.Username, password, k.Adresa, k.Vozilo, DateTime.Now.AddYears(1));
+
+            Korisnici.Remove(k);
+            Korisnici.Add(c);
+
+            return password;
+        }
+
+        public double DajUkupnuMogućuDobitZaKorisnika(Clan c)
+        {
+            if (Lokacije.Count < 1)
+                throw new ArgumentNullException("Nema nijedne lokacije!");
+
+            double zarada = 0;
+            
+            foreach (Lokacija l in Lokacije)
+            {
+                zarada += l.Cijena * l.Ulice.Count;
+            }
+
+            zarada /= c.Vozilo.BrojSjedišta;
+
+            return zarada;
+        }
+
+        #endregion
+    }
+}
---
+++ Grupa 7/Parking Lot/Parking Lot/Vozilo.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Parking_Lot
+{
+    public class Vozilo
+    {
+
+        #region Atributi
+
+        string vrsta, tablice;
+        int brojSjedišta;
+
+        #endregion
+
+        #region Properties
+
+        public string Vrsta
+        {
+            get => vrsta;
+            set
+            {
+                List<string> vrste = new List<string>() { "Automobil", "Motor", "Autobus", "Kamionet", "Minibus" };
+                if (vrste.Find(element => element == value) == null)
+                    throw new ArgumentException("Nepostojeća vrsta vozila!");
+
+                vrsta = value;
+            }
+        }
+
+        public string Tablice
+        {
+            get => tablice;
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value) || value.Length != 9
+                    || !value.Substring(0, 3).All(char.IsDigit)
+                    || !(value.Substring(3, 1) == "-")
+                    || !value.Substring(4, 1).All(char.IsUpper)
+                    || !(value.Substring(5, 1) == "-")
+                    || !value.Substring(6).All(char.IsDigit))
+                    throw new FormatException("Neispravan format tablica!");
+
+                tablice = value;
+            }
+        }
+
+        public int BrojSjedišta
+        {
+            get => brojSjedišta;
+            set
+            {
+                if (value < 0)
+                    throw new FormatException("Neispravan broj sjedišta!");
+
+                brojSjedišta = value;
+            }
+        }
+
+        #endregion
+
+        #region Konstruktor
+
+        public Vozilo(string type, string plates, int noSeats)
+        {
+            Vrsta = type;
+            Tablice = plates;
+            BrojSjedišta = noSeats;
+        }
+
+        #endregion
+
+        #region Metode
+
+        /// <summary>
+        /// Metoda u kojoj se provjerava ispravnost podataka za vozilo.
+        /// Ovisno o vrsti, dozvoljen je različit broj sjedišta:
+        /// Automobil - 4 do 7 sjedišta
+        /// Motor - 1 do 2 sjedišta
+        /// Autobus - 20 do 100 sjedišta
+        /// Kamionet - 2 do 3 sjedišta
+        /// Minibus - 5 do 20 sjedišta
+        /// Sve ostale kombinacije dovode do pojave izuzetka.
+        /// </summary>
+        /// <returns></returns>
+        public bool ProvjeriIspravnostVozila()
+        {
+            bool ok = false;
+            if (Vrsta == "Automobil" && BrojSjedišta > 3 && BrojSjedišta < 8)
+                ok = true;
+            else if (Vrsta == "Motor" && BrojSjedišta > 0 && BrojSjedišta < 3)
+                ok = true;
+            else if (Vrsta == "Autobus" && BrojSjedišta > 19 && BrojSjedišta < 101)
+                ok = true;
+            else if (Vrsta == "Kamionet" && BrojSjedišta > 1 && BrojSjedišta < 4)
+                ok = true;
+            else if (Vrsta == "Minibus" && BrojSjedišta > 4 && BrojSjedišta < 21)
+                ok = true;
+
+            if (!ok)
+                throw new Exception("Vozilo nije ispravno!");
+
+            return ok;
+        }
+
+        #endregion
+    }
+}
